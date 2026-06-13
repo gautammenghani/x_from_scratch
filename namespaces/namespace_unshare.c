3c979eb4c6d64e773916080a259976df30d8140e
Gautam Menghani <gautam.opensource@gmail.com>
2026-06-14 00:27:07 +0530

    cleanups

--- namespaces/namespace_unshare.c
+++ namespaces/namespace_unshare.c
@@ -3,15 +3,6 @@
 #include <stdlib.h>
 #include <sched.h>
 #include <unistd.h>
-#include <string.h>
-#include <assert.h>
-#include <err.h>
-#include <sys/wait.h>
-#include <sys/mount.h>
-#include <sys/syscall.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <linux/sched.h>
 
 int main(int argc, char **argv) {
     if (unshare(CLONE_NEWNET) == -1) {
